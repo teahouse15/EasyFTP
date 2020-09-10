ba2a50385ee1741f5d717d5e2c2cabaa31ac243d
bogendihong <pasterziv@gmail.com>
2020-09-10 17:16:44 +0800

    Update 2020/09/10

--- src/myjinji/Client.java
+++ src/myjinji/Client.java
@@ -26,8 +26,7 @@ public class Client {
             String arg = null;
             try {
                 arg = command.split(" ")[1];
-            } catch (ArrayIndexOutOfBoundsException e) {
-            }
+            } catch (ArrayIndexOutOfBoundsException e) { }
 
             if (cmd.equals("help")) {
                 if (null != arg) {
@@ -66,6 +65,12 @@ public class Client {
                 return;
             } else if (cmd.equals("cd")) {
                 Command.changeDirectory(connector, arg);
+            } else if (cmd.equals("locate")) {
+                if (null != arg) {
+                    Command.locate(connector, arg, "/");
+                } else {
+                    Helper.cmdHelper("locate");
+                }
             } else if (cmd.equals("tree")) {
                 Command.tree(connector, "/", 0);
             } else {
